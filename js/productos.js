4ba723def6b16f842a1a1e89fd66244ee1927531
SamuelMiraG <sam.m0528g@gmail.com>
2024-11-15 00:24:45 -0500

    se agrega los productos para los desayos y almuerzos

--- js/productos.js
+++ js/productos.js
@@ -55,13 +55,46 @@ let loadMoreBtn1= document.querySelector('#cargar-mas-1');
 
 let currentItem1= 4;
 loadMoreBtn1.onclick= () =>{
-    let boxes=[...document.querySelectorAll('.box-container-1 .box-1')];
+    let boxes1=[...document.querySelectorAll('.box-container-1 .box-1')];
     for(var i=currentItem1; i<currentItem1+4;i++){
-        boxes[i].style.display='inline-block';
+        boxes1[i].style.display='inline-block';
     }
     currentItem1+=4;
-    if (currentItem1>=boxes.length) {
+    if (currentItem1>=boxes1.length) {
         loadMoreBtn1.style.display='none'
+        
+    }
+}
+
+
+let loadMoreBtn2= document.querySelector('#cargar-mas-2');
+
+let currentItem2= 4;
+loadMoreBtn2.onclick= () =>{
+    let boxes2=[...document.querySelectorAll('.box-container-2 .box-2')];
+    for(var i=currentItem2; i<currentItem2+4;i++){
+        boxes2[i].style.display='inline-block';
+    }
+    currentItem2+=4;
+    if (currentItem2>=boxes2.length) {
+        loadMoreBtn2.style.display='none'
+        
+    }
+}
+
+
+
+let loadMoreBtn3= document.querySelector('#cargar-mas-3');
+
+let currentItem3= 4;
+loadMoreBtn3.onclick= () =>{
+    let boxes3=[...document.querySelectorAll('.box-container-3 .box-3')];
+    for(var i=currentItem3; i<currentItem3+4;i++){
+        boxes3[i].style.display='inline-block';
+    }
+    currentItem3+=4;
+    if (currentItem3>=boxes3.length) {
+        loadMoreBtn3.style.display='none'
         
     }
 }
